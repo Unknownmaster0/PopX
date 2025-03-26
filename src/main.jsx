208309f5a1327ffc700db6a45daa9c3ca07ec14b
Unknownmaster0 <sagar192002singh@gmail.com>
2025-03-26 18:37:28 +0530

    updated to look like mobile interface in web page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <div className="flex justify-center items-center py-4">
+      <div className="w-[375px] bg-white shadow-lg">
+        <App />
+      </div>
+    </div>
+  </StrictMode>
+);
